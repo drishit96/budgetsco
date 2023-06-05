a1032835778c3fbedda773d1351e57a8b6d08a5f
Drishit Mitra <drishitmitra@gmail.com>
2023-06-05 19:43:00 +0530

    temp check

--- tests/settings.spec.ts
+++ tests/settings.spec.ts
@@ -1,6 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test("can edit budget", async ({ page }) => {
+  await page.goto("http://127.0.0.1:3000/auth/login");
+
+  await page.getByLabel("E-mail Id").fill("#{TEST_LOGIN_USERNAME}#");
+  await page.getByLabel("Password").fill("#{TEST_LOGIN_PASSWORD}#");
+  await page.getByRole("button", { name: "Log in" }).click();
+  await page.waitForURL("**/dashboard", { timeout: 60000 });
+
   await page.goto("/dashboard");
   await page.getByRole("link", { name: "Settings" }).click();
   await page.getByRole("link", { name: "Edit Budget" }).click();
