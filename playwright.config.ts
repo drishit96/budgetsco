dc25710bcfc7cac7e0f2d550c96d388fd93d08f9
Drishit Mitra <drishitmitra@gmail.com>
2023-10-15 13:22:53 +0530

    Increase global timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ const config: PlaywrightTestConfig = {
   testDir: "./tests",
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
-  globalTimeout: 60 * 1000,
+  globalTimeout: 120 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
