698e89c76bb03af6be60f3de047fc30f1f7c4ac9
Drishit Mitra <drishitmitra@gmail.com>
2026-06-03 12:07:29 +0530

    fix: e2e tests

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -2,10 +2,19 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 import { PrismaClient } from "~/generated/prisma/client";
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter, log: ["error", "warn"] });
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "development") (global as any).prisma = prisma;
+if (process.env.NODE_ENV === "production") {
+  const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  prisma = new PrismaClient({ adapter, log: ["error", "warn"] });
+} else {
+  if (!global.prisma) {
+    const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+    global.prisma = new PrismaClient({ adapter, log: ["error", "warn"] });
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
